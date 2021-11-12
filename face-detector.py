077024e8e8046666128915faa0f3d22e2873a512
BullInBoots <90429771+BullInBoots@users.noreply.github.com>
2021-11-12 08:43:46 +0700

    Update face-detector.py

--- face-detector.py
+++ face-detector.py
@@ -1,14 +1,18 @@
 import cv2
+from random import randrange
 
-trained_face_data = cv2.CascadeClassifier(C:\Users\Phern\Desktop\Finn's Folder\dot-xml-files)
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread("RDJ.jpg")
+webcam = cv2.VideoCapture(0)
 
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+while True:
+    successful_frame_read, frame = webcam.read()
 
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2)
 
-print(face_coordinates)
-# cv2.imshow('Face', grayscaled_img)
-cv2.waitKey()
-print("Code completed")
+    cv2.imshow('Face', frame)
+    cv2.waitKey(1)
